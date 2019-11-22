b3f889611956182197751bc62548b7f8adeb5846
Your Name <you@example.com>
2019-11-22 20:31:55 -0300

    Rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListaMateriaisComponent } from './lista-materiais/lista-materiais.component';
+import { NovoMateriaisComponent } from './novo-materiais/novo-materiais.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{ path: 'listagem', component: ListaMateriaisComponent},
+{path: 'novo', component: NovoMateriaisComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
